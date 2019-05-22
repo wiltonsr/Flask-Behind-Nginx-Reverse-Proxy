c5cd748e04a0bddbefad59040f388c706c3cd636
Wilton Rodrigues <wilton.rodrigues@4linux.com.br>
2019-05-22 16:50:26 -0300

    Initial commit

---
+++ flask/app/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', url=request.url)
+
+@app.route('/test')
+def test():
+    return render_template('test.html', url=request.url)
